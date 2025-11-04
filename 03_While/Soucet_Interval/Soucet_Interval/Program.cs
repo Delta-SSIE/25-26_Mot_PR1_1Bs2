2fd36229ff3ebceddd7f4ea5d2bb58acd5a45e5e
Jan Mottl <mottja@delta-skola.cz>
2025-11-04 09:30:56 +0100

    kk

---
+++ 03_While/Soucet_Interval/Soucet_Interval/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Soucet_Interval
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej okraj intervalu");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadej druhý okraj intervalu");
+            int y = int.Parse(Console.ReadLine());
+
+            int max = Math.Max(x, y); //uloží to do max větší z čísel x a y
+            int min = Math.Min(x, y); //uloží to do min menší z čísel x a y
+
+            int soucet = 0;
+
+            while(min <= max)
+            {
+                soucet += min;
+                min++;
+            }
+            Console.WriteLine($"Součet čísel v intervalu (včetně okrajů) je {soucet}");
+        }
+    }
+}
