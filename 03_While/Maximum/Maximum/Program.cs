bb34861f1331b89a5cc0bdc141e1533bed921e87
Jan Mottl <mottja@delta-skola.cz>
2025-11-11 08:45:34 +0100

    kk

---
+++ 03_While/Maximum/Maximum/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Maximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //proměnná do které budu ukládat číslo zadané uživatelem
+            int cislo = 0;
+            //proměnná pro zapsaní nalezeného maxima
+            int max = 0;
+
+            do
+            {
+                Console.WriteLine("Zadej mi číslo");
+                cislo = int.Parse(Console.ReadLine());
+                //Pokud je nově načtené číslo větší než nalezené maximum, pak se hodnota maxima změní na zadané číslo
+                if(cislo > max)
+                {
+                    max = cislo;
+                }
+
+            } while (cislo > 0);
+            //Cyklus se opakuje dokud je v proměnné cislo uložená hodnota vyšší než 0
+
+            Console.WriteLine($"Nejvyšší zadané číslo je {max}");
+        }
+    }
+}
