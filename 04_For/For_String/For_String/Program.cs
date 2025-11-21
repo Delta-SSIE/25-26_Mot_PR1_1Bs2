c50df3393234dc2b776e4d7fef48fdd1e35d1646
Jan Mottl <mottja@delta-skola.cz>
2025-11-21 12:37:54 +0100

    kk

---
+++ 04_For/For_String/For_String/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace For_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej mi slovo");
+            string slovo = Console.ReadLine();
+
+            //"Ahoj"
+            /* 
+             * A
+             * H
+             * O
+             * J
+             */
+
+            //Program, co vypíše slovo po znacích na řádek
+            for (int i = 0; i < slovo.Length; i++)
+                //slovo.length při slově "Ahoj" je 4
+                //indexy začínají od 0
+            {
+                Console.WriteLine(slovo[i]); //vypiš znak ve slově na i-tém indexu
+                //[ pravý alt + f ; ] pravý alt + g
+            }
+
+            //Vypsat do řádku, pouze každý 2. znak ze slova
+            for (int i = 1; i < slovo.Length; i+=2)
+            {
+                Console.Write(slovo[i]);
+            }
+            Console.WriteLine();
+
+            //Vypsat do řádku, pouze znaky 'o'/'O', nebo 'a'/'A' ze slova
+            for (int i = 0; i < slovo.Length; i++)
+            {
+                if (slovo[i] == 'o' || slovo[i] == 'O' || slovo[i] == 'a' || slovo[i] == 'A')
+                {
+                    Console.Write(slovo[i]);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 04_For/Proc_Obraceny_text/Proc_Obraceny_text/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Proc_Obraceny_text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Ahoj, jak se máš?";
+            string obraceny_text = "";
+
+            for (int i = text.Length-1; i >= 0; i--)
+            {
+                obraceny_text += text[i];
+            }
+
+            Console.WriteLine(obraceny_text);
+        }
+    }
+}
