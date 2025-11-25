6b33fb51bbfc73bbb824b24dedaa8ac999858400
Jan Mottl <mottja@delta-skola.cz>
2025-11-25 09:24:45 +0100

    kk

---
+++ 04_For/Malovani_v_konzoli/Malovani_v_konzoli/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Malovani_v_konzoli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Chci udělat do konzole čáru z hastagů - #######
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write('#');
+            }
+            Console.WriteLine();
+
+            //Přerušovaná čára #-#-#-#
+            for (int i = 0; i < 100; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.Write('#');
+                } else
+                {
+                    Console.Write('-');
+                }
+                
+            }
+            Console.WriteLine();
+
+            //Přerušovaná čára s různým počtem hastagů a spojovníků ####--####--####--####
+           
+            //Stokrát budu opakovat '####--'
+            for (int i = 0; i < 100; i++)
+            {
+                //Console.Write("#####");
+                //Vnitřní for cyklus, co se opakuje 4krát a vypisuje vždy #
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write('#');
+                }
+                //Vnitřní for cyklus, který se opakuje 2krát a vypisuje vždy -
+                for (int n = 0; n < 2; n++)
+                {
+                    Console.Write('-');
+                }
+            }
+            Console.WriteLine();
+
+            //2D čtverec
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
