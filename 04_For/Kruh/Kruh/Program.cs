6d47e7706e1508462d652ed7467fd9aacd66bda2
Jan Mottl <mottja@delta-skola.cz>
2025-11-28 12:58:35 +0100

    kk

---
+++ 04_For/Kruh/Kruh/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Kruh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int radky = 5;
+
+            for (int i = 0; i < radky; i++)
+            {
+                for (int j = 0; j <= i; j++) //tady se musí upravit počet mezer, aby jich na začátku bylo hodně a postupně méně
+                {
+                    Console.Write(' ');
+                }
+                for (int k = 0; k <= i; k++) 
+                {
+                    Console.Write('#');
+                }
+                for (int l = 0; l <= i; l++) //tady se musí upravit počet mezer, aby jich na začátku bylo hodně a postupně méně
+                {
+                    Console.Write(' ');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
