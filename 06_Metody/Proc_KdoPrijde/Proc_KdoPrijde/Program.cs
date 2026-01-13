9b2e1fd319bf6d355c66fad181ddea468abee396
Jan Mottl <mottja@delta-skola.cz>
2026-01-13 08:58:48 +0100

    kk

---
+++ 06_Metody/Proc_KdoPrijde/Proc_KdoPrijde/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Proc_KdoPrijde
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //"Přijde také {jmeno}
+            PrijdeI("Pepa");
+            PrijdeI("Karel");
+            PrijdeI("Eliška");
+
+            Console.WriteLine(PrijdeII("Ivan"));
+            Console.WriteLine(PrijdeII("Ludmila"));
+            Console.WriteLine(PrijdeII("Ulrich"));
+
+        }
+
+        static string PrijdeII(string jmeno)
+        {
+            return $"Přijde také {jmeno}";
+        }
+
+        static void PrijdeI(string jmeno)
+        {
+            Console.WriteLine($"Přijde také {jmeno}");
+        }
+    }
+}
