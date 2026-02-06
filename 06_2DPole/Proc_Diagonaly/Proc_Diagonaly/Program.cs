9298b4b1809b9bd6548d2bd4d759782545cd3dc3
Jan Mottl <mottja@delta-skola.cz>
2026-02-06 12:57:06 +0100

    kk

---
+++ 06_2DPole/Proc_Diagonaly/Proc_Diagonaly/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Proc_Diagonaly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] pole = new string[21, 21];
+            
+            for (int i = 0; i < pole.GetLength(0); i++)
+            {
+                for (int j = 0; j < pole.GetLength(1); j++)
+                {
+                    if(i == j)
+                    {
+                        pole[i, j] = "#";
+                    } else if(j == pole.GetLength(1)-1-i)
+                    {
+                        pole[i, j] = "#";
+                    } else
+                    {
+                        pole[i, j] = " ";
+                    }
+                    
+                }
+            }
+
+            /*
+            for (int i = 0; i < pole.GetLength(0); i++)
+            {
+                pole[i, i] = "#";
+            }
+            */
+            Vypis2Dpole(pole);
+        }
+
+        static void Vypis2Dpole(string[,] pole)
+        {
+            for (int i = 0; i < pole.GetLength(0); i++)
+            {
+                for (int j = 0; j < pole.GetLength(1); j++)
+                {
+                    Console.Write(pole[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
