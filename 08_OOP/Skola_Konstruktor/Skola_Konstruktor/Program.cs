8a5c015982d4fb28843fdf0c1420abb5b550a73e
Jan Mottl <mottja@delta-skola.cz>
2026-03-17 09:01:00 +0100

    kk

---
+++ 08_OOP/Skola_Konstruktor/Skola_Konstruktor/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Skola_Konstruktor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vytvoření objektu z třídy Student
+            Student s01 = new Student("Pepík", 15);
+            Console.WriteLine(s01.Introduction());
+            Student s02 = new Student("Jenda", 24);
+            Console.WriteLine(s02.Introduction());
+            Student s03 = new Student("Tereza", 10);
+            Console.WriteLine(s03.Introduction());
+        }
+    }
+}
