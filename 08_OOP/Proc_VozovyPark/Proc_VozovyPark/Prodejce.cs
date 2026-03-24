4b27d8fe9b5961ef9c6a0546d6bb0fdc43fa2e23
Jan Mottl <mottja@delta-skola.cz>
2026-03-24 09:29:59 +0100

    kk

--- 08_OOP/Proc_VozovyPark/Proc_VozovyPark/Prodejce.cs
+++ 08_OOP/Proc_VozovyPark/Proc_VozovyPark/Prodejce.cs
@@ -8,8 +8,19 @@ namespace Proc_VozovyPark
 {
     internal class Prodejce
     {
+        private int[] indexyAut;
         public string Jmeno { get; set; }
-        public int[] IndexyAut { get; set; }
+        public int[] IndexyAut {
+            get
+            {
+                return indexyAut;
+            }
+            set
+            {
+                indexyAut = value;
+            }
+
+        }
 
         public Prodejce(string jmeno)
         {
@@ -17,12 +28,18 @@ namespace Proc_VozovyPark
             this.IndexyAut = new int[0];
         }
 
+        public void PridejAuto(int index)
+        {
+            Array.Resize(ref indexyAut, indexyAut.Length + 1);
+            IndexyAut[IndexyAut.Length - 1] = index;
+        }
         public void VypisAuta(Vozidlo[] auta)
         {
             for (int i = 0; i < auta.Length; i++)
             {
                 for (int j = 0; j < IndexyAut.Length; j++)
                 {
+                    //Do budoucna kontrola dostupnosti daného auta
                     if (IndexyAut[j] == i)
                     {
                         auta[i].VypisInfo();
