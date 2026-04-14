1fe9e71bdcb13fd8aa85efd5e8c4a9c70d1223f5
Jan Mottl <mottja@delta-skola.cz>
2026-04-14 09:33:48 +0200

    kk

---
+++ 08_OOP/Opakovani_test_Ukoly/Opakovani_test_Ukoly/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Opakovani_test_Ukoly
+{
+    public enum KategoriePrace
+    {
+    VyvojSoftware,
+    Marketing,
+    Ucetnictvi,
+    Grafika
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker[] pracovnici = new Worker[10];
+            for (int i = 0; i < pracovnici.Length; i++)
+            {
+                pracovnici[i] = new Worker();
+            }
+            Task[] ukoly = new Task[50];
+            int pocetZadanychUkolu = 0;
+
+            Console.WriteLine("zadej odbornost");
+            int odbornost = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("zadej pocet hodin");
+            int pocetHodin = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("zadej rozpocet");
+            int rozpocet = Convert.ToInt32(Console.ReadLine());
+
+            ukoly[pocetZadanychUkolu] = new Task(pocetZadanychUkolu, (KategoriePrace)odbornost, pocetHodin, rozpocet);
+            pocetZadanychUkolu++;
+        }
+    }
+}
---
+++ 08_OOP/Opakovani_test_Ukoly/Opakovani_test_Ukoly/Task.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opakovani_test_Ukoly
+{
+    internal class Task
+    {
+        public int ID { get; set; }
+        public KategoriePrace PozadovanaOdbornost { get; set; }
+        private double pocetHodin;
+        public double PocetHodin
+        {
+            get
+            {
+                return (pocetHodin);
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    pocetHodin = 1;
+                }
+                else
+                {
+                    pocetHodin = value;
+                }
+            }
+        }
+        private double rozpocetZakazky;
+        public double RozpocetZakazky
+        {
+            get
+            {
+                return rozpocetZakazky;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    rozpocetZakazky = 0;
+                }
+                else
+                {
+                    rozpocetZakazky = value;
+                }
+            }
+        }
+
+        public Task(int id, KategoriePrace odbornost, double pocetHodin, double rozpocetZakazky)
+        {
+            this.ID = id;
+            this.PozadovanaOdbornost = odbornost;
+            this.PocetHodin = pocetHodin;
+            this.RozpocetZakazky = rozpocetZakazky;
+        }
+
+
+    }
+}
