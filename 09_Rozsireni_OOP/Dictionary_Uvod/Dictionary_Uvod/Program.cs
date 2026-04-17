34f31573398abaff3fe029081a4dfe50e385de5a
Jan Mottl <mottja@delta-skola.cz>
2026-04-17 12:50:50 +0200

    kk

---
+++ 09_Rozsireni_OOP/Dictionary_Uvod/Dictionary_Uvod/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Dictionary_Uvod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Určuji jaký datový typ je klíč a jaký je hodnota
+            Dictionary<string, string> CeskoAnglickySlovnik = new Dictionary<string, string>();
+
+            CeskoAnglickySlovnik.Add("Auto", "Car");
+
+            Console.WriteLine(CeskoAnglickySlovnik["Auto"]);
+
+            Dictionary<string, int> TelefoniSeznam = new Dictionary<string, int>();
+            TelefoniSeznam.Add("Pepa Omáčka", 483111222);
+            TelefoniSeznam.Add("Jindřich Smutný", 483951357);
+
+            Console.WriteLine(TelefoniSeznam["Pepa Omáčka"]);
+
+            //Iterace Dicationary
+            foreach(string klic in TelefoniSeznam.Keys)
+            {
+                Console.WriteLine(klic +": "+ TelefoniSeznam[klic]);
+            }
+            foreach (int hodnota in TelefoniSeznam.Values)
+            {
+                Console.WriteLine(hodnota);
+            }
+            foreach(KeyValuePair<string,int> zaznam in TelefoniSeznam)
+            {
+                Console.WriteLine(zaznam.Key +": "+zaznam.Value);
+            }
+        }
+    }
+}
