8bf1f98487501c1f0087f69250e1a4a1d1591659
Jan Mottl <mottja@delta-skola.cz>
2026-04-24 12:59:36 +0200

    kk

---
+++ 09_Rozsireni_OOP/List_Skola/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> skola = new List<Student>();
+            for (int i = 0; i < 1000; i++)
+            {
+                skola.Add(new Student());
+            }
+            skola[50].Introduction();
+            // aby uzivatel zadal vek a vypsalo to jenom studenty v tom veku
+            Console.WriteLine("zadej vek 14-19");
+            int vek = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < 1000; i++)
+            {
+                if (skola[i].Age == vek)
+                {
+                    skola[i].Introduction();
+                }
+            }
+            // nasimulujeme si konec roku neboli vsichni ctvrtaci smazeme a ostatni tridy povysime o jedna
+            Console.WriteLine("---------------------------------");
+            VypisTridy(skola, Grade.A4);
+
+            for (int i = 0; i < skola.Count; i++)
+            {
+                switch (skola[i].Grade)
+                {
+                    case Grade.A1:
+                        skola[i].Grade = Grade.A2;
+                        break;
+                    case Grade.B1:
+                        skola[i].Grade = Grade.B2;
+                        break;
+                    case Grade.A2:
+                        skola[i].Grade = Grade.A3;
+                        break;
+                    case Grade.B2:
+                        skola[i].Grade = Grade.B3;
+                        break;
+                    case Grade.A3:
+                        skola[i].Grade = Grade.A4;
+                        break;
+                    case Grade.B3:
+                        skola[i].Grade = Grade.B4;
+                        break;
+                    case Grade.A4:
+                        skola.Remove(skola[i]);
+                        i--;
+                        break;
+                    case Grade.B4:
+                        skola.Remove(skola[i]);
+                        i--;
+                        break;
+
+                }
+            }
+            Console.WriteLine("---------------------------------");
+            VypisTridy(skola, Grade.A4);
+            Console.WriteLine(skola.Count);
+
+            //nabor prvaku do kapacity 1000
+            int x = skola.Count;
+            for (int i = 0; i < 1000 - x; i++)
+            {
+                Random rnd = new Random();
+                if (rnd.Next(0,1) == 0)
+                {
+                    skola.Add(new Student(0));
+                }
+                else
+                {
+                    skola.Add(new Student(4));
+                }
+                
+            }
+            Console.WriteLine("---------------------------------");
+            VypisTridy(skola, Grade.A1);
+        }
+        static void VypisTridy(List<Student> studenti, Grade grade)
+        {
+            for (int i = 0; i < studenti.Count; i++)
+            {
+                if (studenti[i].Grade == grade)
+                {
+                    studenti[i].Introduction();
+                }
+            }
+
+        }
+
+    }
+}
---
+++ 09_Rozsireni_OOP/List_Skola/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+
+    
+    public enum Grade
+    {
+        A1,A2,A3,A4,B1,B2,B3,B4
+        
+    }
+    internal class Student
+    {
+        public string Name {  get; set; }
+        public int Age { get; set; }
+        public Grade Grade { get; set; }
+        public void Introduction()
+        {
+            Console.WriteLine($"student's name is {this.Name}, age is {this.Age}, grade {this.Grade}");
+        }
+        
+        public Student(int trida = -1)
+        {
+             Random rnd = new Random();
+                string[] names = new string[]
+            {
+                "Jakub", "Jan", "Tomáš", "Matěj", "Filip", "Adam", "Petr", "Lukáš", "Vojtěch", "Šimon",
+                "Eliška", "Tereza", "Adéla", "Anna", "Natálie", "Ema", "Karolína", "Viktorie", "Kristýna", "Lucie",
+                "Martin", "Ondřej", "David", "Dominik", "Marek", "Daniel", "Štěpán", "Jiří", "Michal", "Patrik",
+                "Barbora", "Veronika", "Nela", "Klára", "Anežka", "Julie", "Zuzana", "Sofie", "Marie", "Hana",
+                "Antonín", "Kryštof", "Vít", "Mikuláš", "Tobiáš", "Matyáš", "Václav", "Jáchym", "Linda", "Elena"
+            };
+            this.Name = names[rnd.Next(0,names.Length)];
+            this.Age = rnd.Next(14, 20);
+            if(trida == -1)
+            {
+                this.Grade = (Grade)rnd.Next(0,8);
+            }
+            else
+            {
+                this.Grade = (Grade)trida;
+            }
+           
+
+
+        }
+
+
+    }
+}
